0f1f58d597e334d0fcc40e7437b5a1dfd47502cf
Charlie  Porth <charlieporth@Charlies-MacBook-Pro-7.local>
2024-02-10 13:42:19 -0600

    First commit & some scripts

---
+++ install-new-cli-apps.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env
+
+brew install curl wget
+brew install bash
+# brew install
---
+++ install-new-mac-apps.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# this a apps to install via homebrew `brew` formaly cask apps
+#
+#
+#
+#
+brew install grishka/grishka/neardrop
+# brew install
